4c748c186f97636f03aea5a607e8e24d7dda7738
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2022-04-07 10:29:43 -0500

    Created a Flask API Route to process form

--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, jsonify, send_file
+from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 import os
 
 
@@ -18,7 +20,23 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    form = UploadForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            description = form.description.data
+            photo = form.photo.data
 
+            #getting the filename of the image uploaded
+            filename = secure_filename(file.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return jsonify(message ="File Upload Successful", filename=filename,
+            description=description)
+        else:
+            errors = form_errors(form)
+            return jsonify(errors=errors)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
